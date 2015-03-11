1d17384ca3885283c3c0d8fae050e540346b4380
KateSwann <kateswann72008@gmail.com>
2015-03-11 23:57:31 +0300

    first initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,109 @@
+    module.exports = function(grunt) {
+
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            options: {
+                loadPath: ['bower_components/foundation/scss']
+            },
+            dist: {
+                options: {
+                    sourcemap: 'none',
+                    style: 'nested'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'stylesheets/scss',
+                    src: ['*.scss'],
+                    dest: 'stylesheets/css',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        watch: {
+            grunt: { 
+                files: ["Gruntfile.js"], 
+                tasks: ["default"],
+                options: {
+                  livereload: true
+                } 
+            },
+
+            sass: {
+                files: ["stylesheets/scss/**/*.scss","bower_components/foundation/scss/foundation/*.scss"],
+                tasks: ["buildCss"],
+                options: {
+                  livereload: true
+                }
+            },
+            
+            script: {
+                files: 'develop/js/**/*.js',
+                tasks: ['buildJs'],
+                options: {
+                  livereload: true
+                }
+            }
+        },
+
+        concat: {
+            options: {
+                separator: ';'
+            },
+
+            script: {
+                src: [
+                    'bower_components/foundation/js/foundation.js',
+                    'bower_components/foundation/js/foundation/foundation.alert.js',
+                    'bower_components/foundation/js/foundation/foundation.abide.js',
+                    'bower_components/foundation/js/foundation/foundation.joyride.js',
+                    'js/develop/script.js'
+                ],
+                dest: 'js/assets/script.js'
+            },
+
+            modernizr: {
+                src: [
+                    'bower_components/modernizr/modernizr.js',
+                    'js/develop/custom.modernizr.js'
+                ],
+                dest: 'js/assets/modernizr.js'
+            }
+        },
+
+        // --------------------------------------
+        // Uglify Configuration
+        // --------------------------------------
+
+        uglify: {
+            dist: {
+                files: {
+                    'js/assets/jquery.min.js': ['bower_components/jquery/dist/jquery.js'],
+                    'js/assets/modernizr.min.js': ['js/assets/modernizr.js'],
+                    'js/assets/script.min.js': ['js/assets/script.js']
+                }
+            }
+        }
+    });
+
+    // -----------------------------------------
+    // Load Grunt tasks
+    // -----------------------------------------
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+
+    // -----------------------------------------
+    // Register Grunt tasks
+    // -----------------------------------------
+
+    grunt.registerTask('buildCss', ['sass']);
+    grunt.registerTask('buildJs', ['concat', 'uglify']);
+    grunt.registerTask('default', ['buildCss', 'buildJs', 'watch']);
+};
